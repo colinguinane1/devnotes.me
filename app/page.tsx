9cf85b52eb74ef6b018a5387b0471cc02c62c7e7
colinguinane1 <colinguinane@gmail.com>
2024-07-16 10:58:33 -0600

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return;
+  return (
+    <div className="grid place-content-center h-40 text-4xl font-semibold">
+      Blogchain Home
+    </div>
+  );
 }
