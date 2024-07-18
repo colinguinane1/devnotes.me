a58450e688d12791a7ae36b69ad45568cfba1a7c
colinguinane1 <colinguinane@gmail.com>
2024-07-17 22:05:09 -0600

    added user button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import { useTheme } from "next-themes";
 import { ModeToggle } from "./ThemeSwitcher";
 import { Button } from "@/components/ui/button";
+import { User } from "@clerk/nextjs/server";
+
 const Header = () => {
   const logoText = "<b/>";
+
   return (
     <div className="fixed w-full px-4 top-0 py-3 border-b">
       <ul className="flex items-center  text-lg font-semibold justify-between">
@@ -27,6 +30,9 @@ const Header = () => {
               <SignInButton />
             </Button>
           </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>{" "}
         <button className="border md:hidden rounded-md">
           <BiMenu size={25} color="" />
