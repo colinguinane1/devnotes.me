c07cc3d802f5f0058176b70bd6c047b0fe01769d
colinguinane1 <colinguinane@gmail.com>
2024-07-19 14:01:05 -0600

    added more functinality to header

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -31,16 +31,19 @@ const Sidebar = () => {
               </h1>
             </SheetTitle>
             <SheetDescription>
-              <div className=" gap-2 flex flex-col">
-                {NavigationData.map((nav, index) => (
-                  <Button variant={"secondary"} key={index}>
-                    <Link href={nav.href}>{nav.name}</Link>
-                  </Button>
-                ))}{" "}
-                <div className="flex justify-between items-center">
-                  {" "}
-                  <ModeToggle />
-                  <SignInManager />
+              <div className="flex w-full h-full justify-between ">
+                <div className=" gap-2 justify-between h-full w-full flex flex-col">
+                  {NavigationData.map((nav, index) => (
+                    <Button variant={"secondary"} key={index}>
+                      <Link href={nav.href}>{nav.name}</Link>
+                    </Button>
+                  ))}{" "}
+                  <div className="h-full border-b"></div>
+                  <div className="flex justify-between items-center">
+                    {" "}
+                    <ModeToggle />
+                    <SignInManager />
+                  </div>
                 </div>
               </div>
             </SheetDescription>
--- components/ui/SignInManager.tsx
+++ components/ui/SignInManager.tsx
@@ -1,8 +1,24 @@
-import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignedOut,
+  SignedIn,
+  UserButton,
+  SignOutButton,
+} from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
-const SignInManager = () => {
+export default async function SignInManager() {
+  const user = await currentUser();
+  const userImage = await user?.imageUrl;
   return (
     <>
       <SignedOut>
@@ -11,12 +27,32 @@ const SignInManager = () => {
         </Button>
       </SignedOut>
       <SignedIn>
-        <Button variant={"secondary"}>
-          {" "}
-          <UserButton showName />
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"secondary"}>
+              {" "}
+              <div className="flex gap-2 items-center">
+                <h1>{user?.fullName}</h1>
+                <Image
+                  className="rounded-full"
+                  //@ts-ignore
+                  src={userImage}
+                  alt="User image"
+                  width={30}
+                  height={30}
+                ></Image>
+              </div>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuItem>Account Settings</DropdownMenuItem>
+            <DropdownMenuItem>Your Blogs</DropdownMenuItem>
+            <DropdownMenuItem>
+              <SignOutButton></SignOutButton>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </SignedIn>
     </>
   );
-};
-export default SignInManager;
+}
