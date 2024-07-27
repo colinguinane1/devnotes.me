d1118bd1a98a8ec943ea2459264c9c399f4059aa
colinguinane1 <colinguinane@gmail.com>
2024-07-27 16:24:01 -0600

    updated middleware to protect /write

--- middleware.ts
+++ middleware.ts
@@ -9,5 +9,7 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+       // Protect your specific routes
+    '/write/:path*',
   ],
 };
