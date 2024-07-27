c4c257d6939c78ff8e57fab4fb84273b2665a437
colinguinane1 <colinguinane@gmail.com>
2024-07-26 23:14:47 -0600

    changed it so clerk username is default

--- app/profile/[[...username]]/page.tsx
+++ app/profile/[[...username]]/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { PrismaClient } from "@prisma/client";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 const prisma = new PrismaClient();
 
@@ -24,16 +25,17 @@ export default async function ProfilePage({
   return (
     <main className=" overflow-y-auto  p-4">
       <div className="">
-        <h1 className="text-center py-3 text-2xl font-bold">
-          {user.first_name} {user.last_name}
-        </h1>
-        <img
+        <Image
           src={user.image_url || "/default-avatar.png"}
           alt="User Avatar"
           className="rounded-full"
           width={100}
           height={100}
         />
+        <h1 className="  font-bold">
+          {user.first_name} {user.last_name}
+        </h1>
+
         <p className="text-center py-3">Username: {user.username}</p>
 
         {/* Add more user profile information as needed */}
