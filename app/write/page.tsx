35ca1ea75e8de92a4d644b12a98b7c572d6fb3d9
colinguinane1 <colinguinane@gmail.com>
2024-07-27 14:57:09 -0600

    changed h1

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col p-6 border max-w-xl w-full gap-6 rounded-md bg-card">
         <div className="flex justify-between">
-          <h1 className="text-4xl font-semibold"> Novel Example</h1>
+          <h1 className="text-4xl font-semibold"> Write Your Blog</h1>
         </div>
         <BlogEditor initialValue={value} onChange={setValue} />
       </div>
