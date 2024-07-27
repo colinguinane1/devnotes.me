3444013751a43bd23f40db6ac6bd6a38f9e2a200
colinguinane1 <colinguinane@gmail.com>
2024-07-27 16:25:59 -0600

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([
+  '/write(.*)',
+  '/account(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: [
@@ -9,7 +18,5 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-       // Protect your specific routes
-    '/write/:path*',
   ],
 };
