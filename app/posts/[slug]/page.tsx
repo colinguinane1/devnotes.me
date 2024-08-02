94d72f9a5285c776c443251dc038c7c81daf917f
colinguinane1 <colinguinane@gmail.com>
2024-08-01 21:36:55 -0600

    trying to fix card

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -32,7 +32,15 @@ export default async function blog({ params }: { params: { slug: string } }) {
         <div className="flex flex-col items-center justify-center">
           <CiWarning color="yellow" size={150}></CiWarning>
           <h1 className="font-semibold text-2xl pb-4">Blog not found!</h1>
-          <p className="pb-4">Double check you&apos;re in the right place...</p>
+          <p className="pb-4 text-center">
+            Double check you&apos;re in the right place, or
+            <Link
+              className="font-bold justify-center underline items-center gap-1 flex"
+              href="/support"
+            >
+              contact support <ArrowRight size={12} />
+            </Link>
+          </p>
           <Button className="flex items-center group w-[10rem] gap-1">
             <Link href="/explore" className="flex items-center gap-1">
               Explore{" "}
