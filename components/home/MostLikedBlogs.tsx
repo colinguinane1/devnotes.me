bd9c28256c13849d1c2b5568b34a0f92496cdb13
colinguinane1 <colinguinane@gmail.com>
2024-08-09 09:50:57 -0600

    added more carousels on home page

--- components/home/MostLikedBlogs.tsx
+++ components/home/MostLikedBlogs.tsx
@@ -2,19 +2,19 @@ import * as React from "react";
 import prisma from "@/prisma/db";
 import BlogCard from "../global/BlogCard";
 
-export default async function TopBlogCarousel() {
+export default async function MostLikedBlogs() {
   const posts = await prisma.post.findMany({
     include: {
       author: true,
     },
     orderBy: {
-      views: "desc",
+      likes: "desc",
     },
   });
 
   return (
-    <div className="w-full overflow-hidden overflow-x-auto p-4">
-      <h1 className="text-left text-2xl">Top Blogs</h1>
+    <div className="w-full overflow-x-auto p-4">
+      <h1 className="text-left text-2xl">Most Liked</h1>
       <div className="flex gap-4 py-4 overflow-x-auto overflow-y-hidden h-[17rem]">
         {posts.map((post) => (
           <BlogCard key={post.id} post={post} author={post.author} />
