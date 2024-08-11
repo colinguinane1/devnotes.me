f53770fa48d1062fc067ec09ae91973c0346e36d
colinguinane1 <colinguinane@gmail.com>
2024-08-11 04:26:32 -0600

    updated navbar

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -9,32 +9,41 @@ import { Logo } from "@/data/NavigationData";
 
 const Header = () => {
   return (
-    <div className="fixed w-screen  z-30 bg-background px-2 top-0 py-3 border-b border-secondary">
-      <ul className="flex items-center  text-lg font-semibold justify-between">
-        <div className="">
-          <Link href="/">
-            <h1 className="font-extrabold dark:text-secondary-dark text-primary-light text-blue-400">
-              {Logo}
-            </h1>
-          </Link>
-        </div>
-        <div className=" gap-4  hidden md:flex">
-          {NavigationData.map((nav, index) => (
-            <Link
-              className=" transition hover:text-blue-500 dark:hover:text-blue-300 dark:hover:bg-slate-700 hover:bg-slate-200 hover:bg-opacity-40 p-2 px-4 rounded-full "
-              key={index}
-              href={nav.href}
-            >
-              {nav.name}
+    <div className="fixed  w-screen   z-30 p-2    top-0 py-3 ">
+      <div className="bg-card relative  border-b rounded-lg py-2 px-2 border-secondary">
+        <ul className="flex items-center  text-lg font-semibold justify-between">
+          <div className="hidden md:block ">
+            <Link href="/">
+              <h1 className="font-extrabold dark:text-secondary-dark text-primary-light text-blue-400">
+                {Logo}
+              </h1>
             </Link>
-          ))}
-        </div>
-        <div className="hidden items-center md:flex gap-2">
-          <ModeToggle />
-          <SignInManager />
-        </div>
-        <Sidebar />
-      </ul>
+          </div>
+          <Sidebar />
+          <div className="absolute md:hidden flex items-center justify-center w-full">
+            <Link href="/">
+              <h1 className="font-extrabold dark:text-secondary-dark text-primary-light text-blue-400">
+                devnotes
+              </h1>
+            </Link>
+          </div>
+          <div className=" gap-4  hidden md:flex">
+            {NavigationData.map((nav, index) => (
+              <Link
+                className=" transition hover:text-blue-500 dark:hover:text-blue-300 dark:hover:bg-slate-700 hover:bg-slate-200 hover:bg-opacity-40 p-2 px-4 rounded-full "
+                key={index}
+                href={nav.href}
+              >
+                {nav.name}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden items-center md:flex gap-2">
+            <ModeToggle />
+            <SignInManager />
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
