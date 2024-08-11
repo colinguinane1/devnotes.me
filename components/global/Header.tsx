2011f85f9028bd50b38d698e29aa4cf4617d200c
colinguinane1 <colinguinane@gmail.com>
2024-08-10 21:24:11 -0600

    changes

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -9,32 +9,34 @@ import { Logo } from "@/data/NavigationData";
 
 const Header = () => {
   return (
-    <div className="fixed w-screen  z-30 bg-background px-2 top-0 py-3 border-b border-secondary">
-      <ul className="flex items-center  text-lg font-semibold justify-between">
-        <div className="">
-          <Link href="/">
-            <h1 className="font-extrabold dark:text-secondary-dark text-primary-light text-blue-400">
-              {Logo}
-            </h1>
-          </Link>
-        </div>
-        <div className=" gap-4  hidden md:flex">
-          {NavigationData.map((nav, index) => (
-            <Link
-              className=" transition hover:text-blue-500 dark:hover:text-blue-300 dark:hover:bg-slate-700 hover:bg-slate-200 hover:bg-opacity-40 p-2 px-4 rounded-full "
-              key={index}
-              href={nav.href}
-            >
-              {nav.name}
+    <div className="flex items-center justify-center    z-30 bg-background  px-2  py-3 border-b shadow-lg border-secondary">
+      <div className=" w-full">
+        <ul className="flex items-center   text-lg font-semibold justify-between">
+          <div className="">
+            <Link href="/">
+              <h1 className="font-extrabold dark:text-secondary-dark text-primary-light text-blue-400">
+                {Logo}
+              </h1>
             </Link>
-          ))}
-        </div>
-        <div className="hidden items-center md:flex gap-2">
-          <ModeToggle />
-          <SignInManager />
-        </div>
-        <Sidebar />
-      </ul>
+          </div>
+          <div className=" gap-4  hidden md:flex">
+            {NavigationData.map((nav, index) => (
+              <Link
+                className=" transition hover:text-blue-500 dark:hover:text-blue-300 dark:hover:bg-slate-700 hover:bg-slate-200 hover:bg-opacity-40 p-2 px-4 rounded-full "
+                key={index}
+                href={nav.href}
+              >
+                {nav.name}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden items-center md:flex gap-2">
+            <ModeToggle />
+            <SignInManager />
+          </div>
+          <Sidebar />
+        </ul>
+      </div>
     </div>
   );
 };
