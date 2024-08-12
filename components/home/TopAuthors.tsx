b5c668e4d3dbe02cc48732313e4dd8f38abd0a5c
colinguinane1 <colinguinane@gmail.com>
2024-08-11 22:13:01 -0600

    started implementing userCard's

---
+++ components/home/TopAuthors.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import prisma from "@/prisma/db";
+import UserCard from "../global/UserCard";
+
+export default async function TopAuthors() {
+  const users = await prisma.user.findMany({ orderBy: { created_at: "desc" } });
+
+  return (
+    <div className="w-full overflow-x-auto p-4">
+      <h1 className="text-left text-2xl">Top Authors</h1>
+      <div className="flex gap-4 py-4 overflow-x-auto overflow-y-hidden h-[17rem]">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+}
