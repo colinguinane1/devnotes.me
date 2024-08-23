20136ac90f87f6ad6f537f7cb9650c28539a2eae
colinguinane1 <colinguinane@gmail.com>
2024-08-23 00:18:07 -0600

    fixed deploy issue

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,24 +1,5 @@
 'use server'
 import prisma from "@/prisma/db";
-export async function createPost(data: { title: string; slug: string; content: string; published?: boolean; userId: string, createdAt: Date, description: string, mdx: boolean }) {
-  try {
-    const newPost = await prisma.post.create({
-      data: {
-        title: data.title,
-        description: data.description,
-        slug: data.slug,
-        content: data.content,
-        published: data.published ?? true, 
-        userId: data.userId,
-        createdAt: new Date(), 
-      },
-    });
-    return newPost;
-  } catch (error) {
-    console.error("Error creating post:", error);
-    throw error; 
-  }
-}
 
 export async function incrementViews(postId: string) {
   const post = await prisma.post.findUnique({
