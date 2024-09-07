b5c951fc603b630da22fb111470ee8161dab014a
colinguinane1 <colinguinane@gmail.com>
2024-09-07 16:53:51 -0600

    implemented react-md-editor

--- app/write/advanced/page.tsx
+++ app/write/advanced/page.tsx
@@ -1,37 +1,16 @@
-import Image from "next/image";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
+"use client";
+import React from "react";
+import MDEditor from "@uiw/react-md-editor";
 
-const EditorComp = dynamic(() => import("./EditorComponent"), { ssr: false });
-
-const markdown = `
-Hello **world**!
-`;
-
-export default function Home() {
+export default function App() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
   return (
-    <>
-      <p>
-        This is a bare-bones unstyled MDX editor without any plugins and no
-        toolbar. Check the EditorComponent.tsx file for the code.
-      </p>
-      <p>
-        To enable more features, add the respective plugins to your instance -
-        see{" "}
-        <a
-          className="text-blue-600"
-          href="https://mdxeditor.dev/editor/docs/getting-started"
-        >
-          the docs
-        </a>{" "}
-        for more details.
-      </p>
-      <br />
-      <div style={{ border: "1px solid black" }}>
-        <Suspense fallback={null}>
-          <EditorComp markdown={markdown} />
-        </Suspense>
-      </div>
-    </>
+    <div className="p-4 min-h-screen my-auto">
+      <MDEditor
+        value={value}
+        onChange={(newValue) => setValue(newValue || "")}
+      />
+      <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
+    </div>
   );
 }
