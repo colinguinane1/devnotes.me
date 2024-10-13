7eb18fcd9928fc758103076e20055b0f788434f1
Colin Guinane <colinguinane@gmail.com>
2024-10-13 10:13:34 -0600

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg  backdrop-blur-md text-card-foreground shadow-sm",
+      "rounded-lg  backdrop-blur-md text-card-foreground",
       className
     )}
     {...props}
