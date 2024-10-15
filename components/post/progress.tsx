10e8704ca8f80060fa17f0687a1debd9ab161817
colinguinane1 <colinguinane@gmail.com>
2024-10-15 01:43:40 -0600

    changed progress bar

--- components/post/progress.tsx
+++ components/post/progress.tsx
@@ -1,6 +1,6 @@
-
 "use client";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const ProgressBar = () => {
   const [scrollPercentage, setScrollPercentage] = useState(0);
@@ -8,7 +8,7 @@ const ProgressBar = () => {
   const handleScroll = () => {
     const scrollY = window.scrollY;
     const documentHeight =
-      document.documentElement.scrollHeight - window.innerHeight;
+      document.documentElement.scrollHeight - window.innerHeight - 600;
 
     if (scrollY > 400) {
       const scrolled = ((scrollY - 400) / (documentHeight - 400)) * 100;
@@ -24,10 +24,28 @@ const ProgressBar = () => {
   }, []);
 
   return (
-    <div
-      style={{ width: `${scrollPercentage}%` }}
-      className={`fixed shadow-2xl left-0 h-[2px] z-50 bg-primary`}
-    />
+    <main className="p-4">
+      <AnimatePresence>
+        {scrollPercentage > 0 && (
+          <motion.div
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            exit={{ y: 100 }}
+            transition={{ duration: 0.3 }}
+            className="fixed bottom-4 left-0 w-full px-4"
+          >
+            <div className="w-full bg-gray-500/80 backdrop-blur-xl h-[6px] rounded-full">
+              <motion.div
+                style={{ width: `${scrollPercentage}%` }}
+                className={`h-[6px] rounded-full backdrop-blur-xl ${
+                  scrollPercentage >= 100 ? "bg-green-500/80" : "bg-primary/80"
+                }`}
+              />
+            </div>
+          </motion.div>
+        )}{" "}
+      </AnimatePresence>
+    </main>
   );
 };
 
